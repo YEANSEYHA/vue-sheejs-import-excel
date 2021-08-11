eb30f4420f908f9337adc8754f789b8451028fdf
Yean Seyha <yeanseyha7@gmail.com>
2021-08-11 22:26:16 +0700

    excel import sheet.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
+<!-- Use preprocessors via the lang attribute! e.g. <template lang="pug"> -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>excel import with vue & sheet.js!</h1>    
+    <div>
+        <input type="file" id="excelFile" @change="excelExport" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"/>
+    </div>
+    <div>
+      {{ this.excelData }}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+var XLSX = require('xlsx');
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      excelData: []
+    };
+  },
+  methods: {
+    excelExport(event) {
+      var input = event.target;
+      var reader = new FileReader();
+      reader.onload = () => {
+        var fileData = reader.result;
+        var wb = XLSX.read(fileData, {type : 'binary'});
+        wb.SheetNames.forEach((sheetName) => {
+            /* eslint-disable */
+            var rowObj =XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+            this.excelData = JSON.stringify(rowObj)
+            console.log(rowObj)
+            /* eslint-enable */
+        })
+      };
+      reader.readAsBinaryString(input.files[0]);
+    }
   }
-}
+};
 </script>
 
+<!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
